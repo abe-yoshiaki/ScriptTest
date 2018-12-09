a02d2f3a484c4ffa610406a5bcb8997eb25f0384
Siann55 <takodo_exword22@yahoo.co.jp>
2018-12-09 13:32:00 +0900

    Lesson4の課題提出

---
+++ Assets/Test.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss{
+	private int mp = 53;
+
+	public void magic(){
+		this.mp -= 5;
+
+		if (this.mp < 0) {
+			Debug.Log ("MPが足りないため魔法が使えない。");
+			return;
+		}
+		Debug.Log ("魔法攻撃をした。残りMPは" + this.mp + "。");
+	}
+}
+
+public class Test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		//課題:配列arrayを初期化
+		int[] array = { 10, 20, 30, 40, 50 };
+
+		//課題:配列要素を順番に表示
+		for (int i = 0; i < array.Length; i++) {
+			Debug.Log (array [i]);
+		}
+
+		//課題:配列要素を逆順に表示
+		for (int i = array.Length -1; 0 <= i; i--) {
+			Debug.Log (array [i]);
+		}
+
+		Boss lastboss = new Boss();
+
+		for (int i = 0; i <= 10; i++) {
+			lastboss.magic();
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
